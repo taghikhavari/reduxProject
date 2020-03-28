50d8944c121247a9d82c53c3129b2ce94ace39a5
TaghiGit <mtkhavari@gmail.com>
2020-03-28 17:47:17 +0430

    completed reducers

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,29 +1,49 @@
-export const ADD_TODO = 'ADD_TODO';
-export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
 
-export enum VisibilityFilters {
-  SHOW_ALL= 'SHOW_ALL',
-  SHOW_COMPLETED= 'SHOW_COMPLETED',
-  SHOW_ACTIVE= 'SHOW_ACTIVE',
+export enum EVisibilityFilters {
+	SHOW_ALL = "SHOW_ALL",
+	SHOW_COMPLETED = "SHOW_COMPLETED",
+	SHOW_ACTIVE = "SHOW_ACTIVE"
 }
 
-export function addTodo(text: string){
-  return {
-    type: ADD_TODO,
-    text
-  }
+interface IAddTodo {
+	type: typeof ADD_TODO;
+	text: string;
 }
 
-export function toggleTodo(index: number){
-  return {
-    type: TOGGLE_TODO,
-    index
-  }
+interface IToggleTodo {
+	type: typeof TOGGLE_TODO;
+	index: number;
 }
 
-export function setVisibilityFilter(filter: VisibilityFilters){
-  return {
-    type: VisibilityFilters,
-    filter
-  }
+interface IVisibilityFilterAction {
+	type: typeof SET_VISIBILITY_FILTER;
+	filter: EVisibilityFilters;
 }
+
+export function addTodo(text: string): IAddTodo {
+	return {
+		type: ADD_TODO,
+		text
+	};
+}
+
+export function toggleTodo(index: number): IToggleTodo {
+	return {
+		type: TOGGLE_TODO,
+		index
+	};
+}
+
+export function setVisibilityFilter(
+	filter: EVisibilityFilters
+): IVisibilityFilterAction {
+	return {
+		type: SET_VISIBILITY_FILTER,
+		filter
+	};
+}
+
+export type todoActionTypes = IAddTodo | IToggleTodo | IVisibilityFilterAction;
---
+++ src/reducers/index.ts
@@ -0,0 +1,72 @@
+import {
+	todoActionTypes,
+	EVisibilityFilters,
+	SET_VISIBILITY_FILTER,
+	ADD_TODO,
+	TOGGLE_TODO
+} from "../actions";
+import { combineReducers } from "redux";
+
+interface todo {
+	text: string;
+	completed: boolean;
+}
+
+interface IInitialState {
+	visibilityFilter: EVisibilityFilters;
+	todos: todo[];
+}
+
+const initialState: IInitialState = {
+	visibilityFilter: EVisibilityFilters.SHOW_ALL,
+	todos: [
+		{
+			text: "Consider using Redux",
+			completed: true
+		},
+		{
+			text: "Keep all state in a single tree",
+			completed: false
+		}
+	]
+};
+
+function todoReducer(state: todo[] = [], action: todoActionTypes) {
+	switch (action.type) {
+		case ADD_TODO:
+			return [
+				...state,
+				{
+					text: action.type,
+					completed: false
+				}
+			];
+		case TOGGLE_TODO:
+			return state.map((todo, index) => {
+				if (index === action.index) {
+					return {
+						...todo,
+						completed: !todo.completed
+					};
+				}
+				return todo;
+			});
+	}
+}
+
+function visibilityFilterReducer(
+	state: EVisibilityFilters = EVisibilityFilters.SHOW_ALL,
+	action: todoActionTypes
+) {
+	switch (action.type) {
+		case SET_VISIBILITY_FILTER:
+			return action.filter;
+		default:
+			return state;
+	}
+}
+
+export const todoAppReducer = combineReducers({
+  visibilityFilterReducer,
+  todoReducer
+})
